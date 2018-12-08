a4638205ca339919a9efb68ce20c7ace59a53d90
stevegleds <stevegleds@gmail.com>
2018-12-08 20:34:32 +0000

    08 December 2018 Created menu so that once the output file is created I can work on analysing without repeating file creation

--- main.py
+++ main.py
@@ -12,28 +12,60 @@ print('Output file used is:', OUTPUT_FILE)
 
 
 def main():
-    speed_data = parse(DATA_FILE, ',')
-    print('Speed data is:', speed_data)
-    countries = parse(CONSTANTS_FILE, ',')
-    for country in countries:
-        print(country['Radius'])
-    print('The countries are:', countries)
-    print('The countries are: ', countries)
-    #  city_results = 0
-    results = addcitypeaktimedata(countries, speed_data)
-    #  results = speed_data  # results is same as input for testing only
-    save_results(OUTPUT_FILE, results)
-    df = pd.read_csv(OUTPUT_FILE)
-    print(df.head())
-    pivot = pd.pivot_table(df, index=["Country", "New City", "New Peak", "ConnectionType"], values=["Download"],
+    ans = True
+    while ans:
+        print('''
+        Process new data file:
+        1 Parse source file to internal dict
+        2 Parse constants file to internal countries dict
+        3 Add City and Peak time columns to results and create output file
+        
+        Analyse output file:
+        4 Create Pivot table Country > City > Peak > Type
+        5 Send Pivot to pivot file
+        6 Testing options
+        ''')
+        ans = int(input('What do you want?'))
+        if ans == 1:
+            speed_data = parse(DATA_FILE, ',')
+        if ans == 2:
+            countries = parse(CONSTANTS_FILE, ',')
+        if ans == 3:
+            results = addcitypeaktimedata(countries, speed_data)
+            save_results(OUTPUT_FILE, results)
+        if ans == 4:
+            df = pd.read_csv(OUTPUT_FILE)
+            print(df.head())
+            pivot = pd.pivot_table(df, index=["Country", "New City", "New Peak", "ConnectionType"], values=["Download"],
                            aggfunc=['count', 'sum', 'mean', 'median'])
-    print(pivot)
-    with open(PIVOT_FILE, "w", newline='') as f:
-        pivot.to_csv(f)
-    f.close()
+            print(pivot)
+        if ans == 5:
+            with open(PIVOT_FILE, "w", newline='') as f:
+                pivot.to_csv(f)
+            f.close()
+        if ans == 6:
+            print(df['Country'])
+            pass
 
-
-
+    # speed_data = parse(DATA_FILE, ',')
+    # print('Speed data is:', speed_data)
+    # countries = parse(CONSTANTS_FILE, ',')
+    # for country in countries:
+    #     print(country['Radius'])
+    # print('The countries are:', countries)
+    # print('The countries are: ', countries)
+    # #  city_results = 0
+    # results = addcitypeaktimedata(countries, speed_data)
+    # #  results = speed_data  # results is same as input for testing only
+    # save_results(OUTPUT_FILE, results)
+    # df = pd.read_csv(OUTPUT_FILE)
+    # print(df.head())
+    # pivot = pd.pivot_table(df, index=["Country", "New City", "New Peak", "ConnectionType"], values=["Download"],
+    #                        aggfunc=['count', 'sum', 'mean', 'median'])
+    # print(pivot)
+    # with open(PIVOT_FILE, "w", newline='') as f:
+    #     pivot.to_csv(f)
+    # f.close()
 
 if __name__ == "__main__":
     main()
