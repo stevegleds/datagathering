a0c20e47f0ec656a9280b5d5638211323eb03795
Steve Gledhill <stevegleds@gmail.com>
2018-12-14 16:07:24 +0000

    14 December 2018 Was working on getting 3 letter country codes but Diego is modifying the script Tidied up code to comment out unused. Next step is to look at presenting data

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from parse import parse, save_results
-from dataprocessing import findGeoDistance, incity, addcitypeaktimedata, getdistance, getpeak, addextradata
+from dataprocessing import addextradata
 import pandas as pd
 
 
-CSV_FILE = '13mydsp2.csv'  # this is the raw data
+CSV_FILE = 'mesample.csv'  # this is the raw data
 EXCEL_FILE = 'mesample.xlsx'
 #  EXCEL_FILE = 'me20181127-01.xlsx'
 OUTPUT_FILE = 'output.csv'  # this is the raw data with fields for city and peak time info
@@ -19,41 +18,17 @@ def main():
     ans = True
     while ans:
         print('''
-        Process new data file:
-        1 Parse source file to internal dict
-        2 Parse constants file to internal countries dict
-        3 Add City and Peak time columns to results and create output file
-        
-        Analyse output file:
-        4 Create Pivot table Country > City > Peak > Type
-        5 Send Pivot to pivot file
-        6 Testing options
-        
-        7 Do it all in pandas
-        8 Create pivot table(s)
+        1 Parse Source File
+        2 Create Pivot table Country > City > Peak > Type
         ''')
         ans = int(input('What do you want?'))
-        if ans == 1:
-            speed_data = parse(CSV_FILE, ',')
-        if ans == 2:
-            countries = parse(CONSTANTS_FILE, ',')
-        if ans == 3:
-            results = addcitypeaktimedata(countries, speed_data)
-            save_results(OUTPUT_FILE, results)
-        if ans == 4:
-            dfresults = pd.read_csv(OUTPUT_FILE)
-            print(dfresults.head())
-            pivot = pd.pivot_table(dfresults, index=["Country", "New City", "New Peak", "ConnectionType"], values=["Download"],
-                           aggfunc=['count', 'sum', 'mean', 'median'])
-            print(pivot)
-        if ans == 5:
-            with open(PIVOT_FILE, "w", newline='') as f:
-                pivot.to_csv(f)
-            f.close()
-        if ans == 6:
-            print(dfresults['Country'])
-            pass
-        if ans == 7:  # do it all in pandas:
+        if ans == 1:  # do it all in pandas:
+            print("Use the following files?")
+            print('Data file used is: ', CSV_FILE)
+            print('Output file used is:', OUTPUT_FILE)
+            response = input("Y to continue; any other key to abort /n" )
+            if not response.lower():
+                pass
             dfcountry = pd.read_csv(CONSTANTS_FILE)
             dfresults = pd.read_csv(CSV_FILE)
             dfresults = addextradata(dfresults, dfcountry)
@@ -63,7 +38,7 @@ def main():
             except:
                 print(OUTPUT_FILE, "is open")
             outputfilecreated = True
-        if ans == 8:
+        if ans == 2:
             if not outputfilecreated:
                 print("Please process raw data file before analysing. Option 7.")
             else:
