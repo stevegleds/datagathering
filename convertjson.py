10b70b5932566fafda4fd63b4e523f1302a208bf
Steve Gledhill <stevegleds@gmail.com>
2019-04-09 11:18:54 +0100

    09 April 2019 Added code to process each json file and then combine the resulting csvs to one combined csv

--- convertjson.py
+++ convertjson.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import os
+import glob
+from pathlib import Path
 
 # Check we are in the correct directory
 print(os.getcwd())
-#  os.chdir("data/json")
-os.chdir("C:/Users/steve/Desktop/work_temp/androidconfig/TestResultsJobs/testresults-20190403160000")
+subpath = "/testresults-20190404000000"  # To be used so we only need to change directory in one place
+csvfilename = subpath[1:] + ".csv"
+path = "S:/pythoncode/myprojects/work/datagathering/data/json/source" + subpath
+os.chdir(path)
 print(os.getcwd())
-# Change json_file csvfile as required for source and destination
-jobID = "08b3c438-a675-4c74-9da4-e4f604b5e934"
-json_file = jobID + ".json"
-csvfile = json_file + '.csv'
-print('csvfile is: ', csvfile)
-print('json file is: ', json_file)
 
 
 #  Unpack dictionary elements and add one column for each dictionary field.
@@ -26,6 +24,27 @@ def unpack(df, column, fillna=None):
     return ret
 
 
+# filename is the json file and this process converts json to dataframe and then exports to csv
+def jsontocsv(filename):
+    df = pd.read_json(filename, lines=True)
+    df = unpack(df, 'HttpTestResults', 0)
+    df = unpack(df, 0, 0)
+    df = unpack(df, 'ResponseStatus', 0)
+    df = unpack(df, 'ProbeInfo', 0)
+    # df = unpack(df, 'TestStatus', 0)
+    cols = df.columns.tolist()
+    new_cols = ['JobID', 'Datacentre', 'Throughput']
+    cols = new_cols + cols
+    print(cols)
+    df['Throughput'] = (df['DownloadedBytes'] * 8) / (df['TotalTime'] / 1000) / 1000000
+    df['JobID'] = jobID
+    df = df.apply(add_datacentre, axis=1)
+    print(df.columns.tolist())
+    df = df[cols]  # Creates df with only those columns in 'cols'. In this case we use all columns but change the order.
+    df.to_csv(csvfile)
+    return df
+
+
 #  Simple function to lookup destination data nad replace with user-friendly data
 def add_datacentre(df):
     if df['Destination'] == "https://kong.speedcheckerapi.com:8443/download/100mb.zip?apikey=1a2b3c4d-1975-1978-9876-9f8e7d6c5b4a":
@@ -37,22 +56,20 @@ def add_datacentre(df):
     return df
 
 
-df = pd.read_json(json_file, lines=True)
-df = unpack(df, 'HttpTestResults', 0)
-df = unpack(df, 0, 0)
-df = unpack(df, 'ResponseStatus', 0)
-df = unpack(df, 'ProbeInfo', 0)
-df = unpack(df, 'TestStatus', 0)
-cols = df.columns.tolist()
-new_cols = ['JobID', 'Datacentre', 'Throughput']
-cols = new_cols + cols
-print(cols)
-df['Throughput'] = (df['DownloadedBytes']*8)/(df['TotalTime']/1000)/1000000
-df['JobID'] = jobID
-df = df.apply(add_datacentre, axis=1)
-print(df.columns.tolist())
-df = df[cols]  # Creates df with only those columns in 'cols'. In this case we use all columns but change the order.
-df.to_csv(csvfile)
+# Go through all json files and create csv files
+for filename in glob.glob(os.path.join(path, '*.bad0404')):  # Uses glob to only use .json files
+    print('Filename is: ', filename)
+    jobID = Path(filename).stem  # returns the filename with no path or extension info
+    print('JobID is: ', jobID)
+    csvfile = jobID + '.csv'
+    jsontocsv(filename)
+
+# go through all csv files and create single csv file
+all_filenames = [i for i in glob.glob('*.{}'.format('csv'))]
+# combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames], sort=False)
+# export to csv
+combined_csv.to_csv(csvfilename, index=False, encoding='utf-8-sig')
 
 
 
